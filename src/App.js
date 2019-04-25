1b334ffbc9c6aee3f9aacdc3778e439b554fabab
rYDer007 <rYDer007@users.noreply.github.com>
2019-04-26 05:15:46 +0530

    :rocket: added button

--- src/App.js
+++ src/App.js
@@ -21,10 +21,10 @@ const Grid = styled.div`
 padding-top: 3em;
 display: grid;
 grid-template-columns: 12em 12em 12em 12em;
-grid-template-rows: 12px 12px  ;
+grid-template-rows: 12px 12px 12px;
   align-items: start;
  
-  grid-gap: 5em;
+  grid-gap: 1em;
   justify-content:space-evenly;
   `;
 
@@ -39,10 +39,14 @@ const Button = styled.button`
   cursor:pointer;
   font-weight: 300;
   opacity: 1;
+  outline: 0;
 
   ${props => props.primary && css`
     background: #ea674d;
     color: white;
+    :hover {
+      opacity: 0.9;
+    }
   `}
   
   ${props => props.disabled && css`
@@ -56,34 +60,56 @@ const Button = styled.button`
   box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
   background: #ea674d;
   color: white;
+  :hover {
+    opacity: 0.9;
+  }
   `}
 
   ${props => props.flat && css`
   border: 0;
   background: white;
+  font-weight: 700;
   `}
 
   ${props => props.hover && css`
   :hover {
     box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
     background: #ea674d;
-  color: white;}
+  color: white;
+transition:0.5s ease-in;
+:hover {
+  opacity: 0.9;
+}
+}
   `}
 
   ${props => props.focus && css`
   :focus {
     box-shadow: 0 0 5px #ea674d;
-    outline: 0;}
+    outline: 0;
+    :hover {
+      opacity: 0.9;
+    }
+  }
   `}
 
   ${props => props.active && css`
   :active:focus {
-    font-weight: bold;}
+    font-weight: bold;:hover {
+      opacity: 0.9;
+    }
+  }
   `}
+
+  ${props => props.round && css`
+  transition:border-radius 0.5s ease-in-out;
+outline: 0;
+  :hover {
+    border-radius: 5em;
+  }`}
+
+  
 `;
-
-
-
 
 
 const App = () => {
@@ -101,9 +127,13 @@ const App = () => {
       </Grid>
       <Grid>
         <Button flat>Flat Button</Button>
-        <Button hover>Hover Button</Button>
-        <Button focus>Focus Button</Button>
+
+        <Button focus primary>Focus Button</Button>
         <Button active primary>Active Button</Button>
+        <Button hover>Hover Button</Button>
+      </Grid>
+      <Grid>
+        <Button primary round>Primary Button</Button>
       </Grid>
 
     </Wrapper>
