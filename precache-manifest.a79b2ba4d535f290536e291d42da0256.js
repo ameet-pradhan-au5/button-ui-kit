015f52db02e455630d8b4690dcf6311aa00286c8
rYDer007 <rYDer007@users.noreply.github.com>
2019-04-26 01:51:38 +0530

    Updates

---
+++ precache-manifest.a79b2ba4d535f290536e291d42da0256.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bc37176173e65154d892d410d6ebc8b",
+    "url": "/button-ui-kit/index.html"
+  },
+  {
+    "revision": "005a746b1d537030d2d8",
+    "url": "/button-ui-kit/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "ce1f9e89af02a4b98b05",
+    "url": "/button-ui-kit/static/js/2.e1b72d62.chunk.js"
+  },
+  {
+    "revision": "005a746b1d537030d2d8",
+    "url": "/button-ui-kit/static/js/main.8d140bd4.chunk.js"
+  },
+  {
+    "revision": "725bd6523051b3bb6ff0",
+    "url": "/button-ui-kit/static/js/runtime~main.50ad2bae.js"
+  }
+]);
