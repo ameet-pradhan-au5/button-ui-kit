b4090a7cdc3d67679fa4489a8c5e9c9a268524d4
rYDer007 <rYDer007@users.noreply.github.com>
2019-04-25 16:59:24 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Title = styled.h1
-  `
+const Title = styled.h1`
   font-size: 1.5em;
   text-align: center;
   color: palevioletred;
 `;
-const Wrapper = styled.section
-  `
+const Wrapper = styled.section`
   padding: 4em;
   background: papayawhip;
 
@@ -19,7 +17,6 @@ const App = () => {
     <Wrapper>
       <Title>Hello World, this is my first styled component!</Title>
     </Wrapper>
-
   );
 }
 
